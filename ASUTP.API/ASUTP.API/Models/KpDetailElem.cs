89ceadadd0a1502184d76e17c906ceb5a596beb4
Kurlin Alexandr <sasha-kurlin@mail.ru>
2023-08-07 19:31:57 +0300

    Добавлены модели KP

---
+++ ASUTP.API/ASUTP.API/Models/KpDetailElem.cs
@@ -0,0 +1,11 @@
+﻿namespace ASUTP.API.Models
+{
+    public class KpDetailElem
+    {
+        public int Id { get; set; }
+        public int ChapterId { get; set; }
+        public int BoundleId { get; set; }
+        public int ParentId { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ ASUTP.API/ASUTP.API/Models/KpMasterElem.cs
@@ -0,0 +1,11 @@
+﻿namespace ASUTP.API.Models
+{
+    public class KpMasterElem
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? Description { get; set; }
+        public int Revision { get; set; }
+
+    }
+}
